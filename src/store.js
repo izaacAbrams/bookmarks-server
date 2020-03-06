60b7c93c110680c769dec4ab8dfeaa0d5e9a6109
izaacAbrams <izaac.abrams25@gmail.com>
2020-03-05 18:25:22 -0600

    added express routes to handle adding and deleting bookmarks

---
+++ src/store.js
@@ -0,0 +1,10 @@
+const bookmarks = [
+  {
+    title: "Bookmark 1",
+    url: "https://github.com",
+    description: "Bookmark 1 for github",
+    rating: 4
+  }
+];
+
+module.exports = { bookmarks };
