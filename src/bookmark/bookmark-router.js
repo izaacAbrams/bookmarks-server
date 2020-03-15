52fba6b0f14e42029b10db3995c62616bfcdbf4a
izaacAbrams <izaac.abrams25@gmail.com>
2020-03-14 21:54:00 -0500

    refactored to use database

--- src/bookmark/bookmark-router.js
+++ src/bookmark/bookmark-router.js
@@ -3,12 +3,18 @@ const bookmarkRouter = express.Router();
 const bodyParser = express.json();
 const uuid = require("uuid/v4");
 const logger = require("../logger");
-const { bookmarks } = require("../store");
+const BookmarksService = require("./bookmarks-service");
 
 bookmarkRouter
   .route("/bookmarks")
-  .get((req, res) => {
-    res.json(bookmarks);
+  .get((req, res, next) => {
+    const knexInstance = req.app.get("db");
+
+    BookmarksService.getAllBookmarks(knexInstance)
+      .then(bookmarks => {
+        res.json(bookmarks);
+      })
+      .catch(next);
   })
   .post(bodyParser, (req, res) => {
     const { title, url, description, rating } = req.body;
@@ -49,16 +55,19 @@ bookmarkRouter
   });
 
 bookmarkRouter
-  .route("/bookmarks/:id")
-  .get((req, res) => {
-    const { id } = req.params;
-    const bookmark = bookmarks.find(b => b.id == id);
-
-    if (!bookmark) {
-      logger.error(`Bookmark with id ${id} not found`);
-      return res.status(404).send("Bookmark not found");
-    }
-    res.json(bookmark);
+  .route("/bookmarks/:bookmark_id")
+  .get((req, res, next) => {
+    const knexInstance = req.app.get("db");
+    BookmarksService.getById(knexInstance, req.params.bookmark_id)
+      .then(bookmark => {
+        if (!bookmark) {
+          return res.status(404).json({
+            error: { message: `Bookmark not found` }
+          });
+        }
+        res.json(bookmark);
+      })
+      .catch(next);
   })
   .delete((req, res) => {
     const { id } = req.params;
---
+++ test/bookmarks.fixtures.js
@@ -0,0 +1,64 @@
+function makeBookmarksArray() {
+  return [
+    {
+      id: 1,
+      title: "First test!",
+      url: "https://github.com/",
+      description: "Github 1",
+      rating: 3
+    },
+    {
+      id: 2,
+      title: "Second test!",
+      url: "https://github.com/2",
+      description: "Github 2",
+      rating: 3
+    },
+    {
+      id: 3,
+      title: "Third test!",
+      url: "https://github.com/3",
+      description: "Github 3",
+      rating: 3
+    },
+    {
+      id: 4,
+      title: "Fourth test!",
+      url: "https://github.com/4",
+      description: "Github 4",
+      rating: 3
+    },
+    {
+      id: 5,
+      title: "Fifth test!",
+      url: "https://github.com/5",
+      description: "Github 5",
+      rating: 3
+    },
+    {
+      id: 6,
+      title: "Sixth test!",
+      url: "https://github.com/6",
+      description: "Github 6",
+      rating: 3
+    },
+    {
+      id: 7,
+      title: "Seventh test!",
+      url: "https://github.com/7",
+      description: "Github 7",
+      rating: 3
+    },
+    {
+      id: 8,
+      title: "First test!",
+      url: "https://github.com/",
+      description: "Github 8",
+      rating: 3
+    }
+  ];
+}
+
+module.exports = {
+  makeBookmarksArray
+};
